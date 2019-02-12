ea513ad552eef2e89ff589e0e4c1ca7e06eeebbd
sysreqx <sysreqx@gmail.com>
2019-02-12 11:05:41 +0600

    printing data tours

--- page-tours.php
+++ page-tours.php
@@ -19,18 +19,41 @@
 					'order'						=> 'ASC',
 				);
 				$query = new WP_Query( $args ); ?>
-
+				
 				<?php if ( $query->have_posts() ) : ?>
 
-					<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-						<h3><?php the_title(); ?></h3>
-					<?php endwhile; ?>
+					<ul>
+						<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+							<li>
+								<div class="featured-tour">
+									<?php the_post_thumbnail(); ?>
+									<a href="<?php get_permalink(); ?>" class="more-info">
+										<img src="<?php echo get_template_directory_uri(); ?>/img/moreinfo.png" alt="<?php echo get_the_title(); ?>">
+									</a>
+								</div>
+								<?php 
+								$dateformatstring = 'j M Y';
+								$unixtimestamp = strtotime(get_field('leaving_date'));
+								$leaving_date = date_i18n( $dateformatstring, $unixtimestamp );
 
-					<?php wp_reset_postdata(); ?>
+								$unixtimestamp = strtotime(get_field('returning_date'));
+								$returning_date = date_i18n( $dateformatstring, $unixtimestamp );
 
-					<?php else : ?>
-						<p><?php esc_html_e( 'Нет постов по вашим критериям.' ); ?></p>
-					<?php endif; ?>
+								?>
+								<div class="date-price">
+									<p class="date"><?php echo $leaving_date . ' - ' . $returning_date ?></p>
+									<p class="price"><?php echo get_field('price') ?></p>
+								</div>
+								<div class="tour-description">
+									<?php the_field('small_description'); ?>
+								</div>
+							</li>
+						<?php endwhile; ?>
+						<?php wp_reset_postdata(); ?>
+						<?php else : ?>
+							<p><?php esc_html_e( 'Нет постов по вашим критериям.' ); ?></p>
+						<?php endif; ?>
+					</ul>
 
 					<?php edit_post_link(); ?>
 
