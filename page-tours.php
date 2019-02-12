af1f564849a996a18d48ea7280ef3ad8fb3bc4f3
sysreqx <sysreqx@gmail.com>
2019-02-12 12:13:47 +0600

    styling the page-tours

--- page-tours.php
+++ page-tours.php
@@ -4,76 +4,82 @@
 <main role="main">
 	<!-- section -->
 	<section>
-
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-			<h2><span><?php the_title(); ?></span></h2>
-
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<?php 
-				$args = array(
-					'post_type'				=> 'atw_tours',
-					'posts_per_page'	=> -1,
-					'order_by'				=> 'title',
-					'order'						=> 'ASC',
-				);
-				$query = new WP_Query( $args ); ?>
-				
-				<?php if ( $query->have_posts() ) : ?>
-
-					<ul>
-						<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-							<li>
-								<div class="featured-tour">
-									<?php the_post_thumbnail(); ?>
-									<a href="<?php get_permalink(); ?>" class="more-info">
-										<img src="<?php echo get_template_directory_uri(); ?>/img/moreinfo.png" alt="<?php echo get_the_title(); ?>">
-									</a>
-								</div>
-								<?php 
-								$dateformatstring = 'j M Y';
-								$unixtimestamp = strtotime(get_field('leaving_date'));
-								$leaving_date = date_i18n( $dateformatstring, $unixtimestamp );
-
-								$unixtimestamp = strtotime(get_field('returning_date'));
-								$returning_date = date_i18n( $dateformatstring, $unixtimestamp );
-
-								?>
-								<div class="date-price">
-									<p class="date"><?php echo $leaving_date . ' - ' . $returning_date ?></p>
-									<p class="price"><?php echo get_field('price') ?></p>
-								</div>
-								<div class="tour-description">
-									<?php the_field('small_description'); ?>
-								</div>
-							</li>
-						<?php endwhile; ?>
-						<?php wp_reset_postdata(); ?>
-						<?php else : ?>
-							<p><?php esc_html_e( 'Нет постов по вашим критериям.' ); ?></p>
-						<?php endif; ?>
-					</ul>
-
-					<?php edit_post_link(); ?>
-
-				</article>
-				<!-- /article -->
-
-			<?php endwhile; ?>
-
-			<?php else: ?>
+		<div class="wrapper">
+			
+			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+				<h2><span><?php the_title(); ?></span></h2>
 
 				<!-- article -->
-				<article>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-					<h2><?php _e( 'Sorry, nothing to display.', 'atw' ); ?></h2>
+					<?php 
+					$args = array(
+						'post_type'				=> 'atw_tours',
+						'posts_per_page'	=> -1,
+						'order_by'				=> 'title',
+						'order'						=> 'ASC',
+					);
+					$query = new WP_Query( $args ); ?>
+					
+					<?php if ( $query->have_posts() ) : ?>
 
-				</article>
-				<!-- /article -->
+						<ul class="tours">
+							<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+								<li>
+									<div class="featured-tour">
+										<?php the_post_thumbnail("featuredTour"); ?>
+										<a href="<?php the_permalink(); ?>" class="more-info">
+											<img src="<?php echo get_template_directory_uri(); ?>/img/moreinfo.png" alt="<?php echo get_the_title(); ?>">
+										</a>
+									</div>
+									<?php 
+									$dateformatstring = 'j M Y';
+									$unixtimestamp = strtotime(get_field('leaving_date'));
+									$leaving_date = date_i18n( $dateformatstring, $unixtimestamp );
 
-			<?php endif; ?>
+									$unixtimestamp = strtotime(get_field('returning_date'));
+									$returning_date = date_i18n( $dateformatstring, $unixtimestamp );
+									?>
+									<h3>
+										<a href="<?php the_permalink(); ?>">
+											<?php the_title(); ?>
+										</a>
+									</h3>
+									<div class="date-price">
+										<p class="date"><?php echo $leaving_date . ' - ' . $returning_date ?></p>
+										<p class="price"><?php echo get_field('price') ?></p>
+									</div>
+									<div class="tour-description">
+										<?php the_field('small_description'); ?>
+									</div>
+								</li>
+							<?php endwhile; ?>
+							<?php wp_reset_postdata(); ?>
+							<?php else : ?>
+								<p><?php esc_html_e( 'Нет постов по вашим критериям.' ); ?></p>
+							<?php endif; ?>
+						</ul>
 
+						<?php edit_post_link(); ?>
+
+					</article>
+					<!-- /article -->
+
+				<?php endwhile; ?>
+
+				<?php else: ?>
+
+					<!-- article -->
+					<article>
+
+						<h2><?php _e( 'Sorry, nothing to display.', 'atw' ); ?></h2>
+
+					</article>
+					<!-- /article -->
+
+				<?php endif; ?>
+
+			</div>
 		</section>
 		<!-- /section -->
 	</main>
